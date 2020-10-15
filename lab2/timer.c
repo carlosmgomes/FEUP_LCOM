67046659a458e5a56d96189cd91a970a8e7acd88
Carlos Gomes <up201906622@fe.up.pt>
2020-10-15 10:28:14 +0100

    Updated Lab2

--- lab2/timer.c
+++ lab2/timer.c
@@ -6,10 +6,44 @@
 #include "i8254.h"
 
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
+uint8_t lsb = 0, msb = 0, status;
+uint16_t cicles = TIMER_FREQ/freq;
+util_get_LSB(cicles,&lsb);
+util_get_MSB(cicles,&msb);
+if(timer_get_conf(timer,&status)!=0){
+  printf("Timer_get_conf error");
   return 1;
+}
+uint8_t timer_addr=0;
+if(timer==0){
+  status = ((status & 0x0f)|TIMER_SEL0|TIMER_LSB_MSB);
+  timer_addr = TIMER_0;
+}
+else if(timer==1){
+  status = ((status & 0x0f)|TIMER_SEL1|TIMER_LSB_MSB);
+    timer_addr = TIMER_1;
+
+}
+else if(timer==2){
+   status = ((status & 0x0f)|TIMER_SEL2|TIMER_LSB_MSB);
+     timer_addr = TIMER_2;
+
+}
+else{
+  printf("Invalid timer\n");
+  return 1;
+  }
+  if(sys_outb(TIMER_CTRL,status)!=0){
+    return 1;
+    }
+  else if(sys_outb(timer_addr,msb)!=0){
+    return 1;
+    }
+  else if(sys_outb(timer_addr,lsb)!=0){
+    return 1;
+    }
+return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -32,16 +66,64 @@ void (timer_int_handler)() {
 }
 
 int (timer_get_conf)(uint8_t timer, uint8_t *st) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
+uint16_t rbcmd = TIMER_RB_CMD | TIMER_RB_COUNT_ | TIMER_RB_SEL(timer);
+if(sys_outb(TIMER_CTRL,rbcmd)){
+  printf("sys outb error\n");
   return 1;
+}
+uint32_t temp;
+
+if(sys_inb(TIMER_0+timer,&temp)){
+  printf("sys inb error\n");
+  return 1;
+}
+*st = temp;
+  return 0;
 }
 
 int (timer_display_conf)(uint8_t timer, uint8_t st,
                         enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
 
-  return 1;
+union timer_status_field_val conf;
+
+switch(field){
+  case tsf_all:
+    conf.byte = st;
+    break;
+  case tsf_initial:
+    switch(tsf_initial){
+      case INVAL_val:
+      conf.in_mode = INVAL_val;
+      break;
+      case LSB_only:
+      conf.in_mode = LSB_only;
+      break;
+      case MSB_only:
+      conf.in_mode = MSB_only;
+      break;
+      case MSB_after_LSB:
+      conf.in_mode = MSB_after_LSB;
+      break;
 }
+  case tsf_mode:
+  //uint8_t temp = ((st & (BIT(1)|BIT(2)|BIT(3)))>>1);
+  switch(((st & (BIT(1)|BIT(2)|BIT(3)))>>1)){
+    case 6:
+    conf.count_mode = 2;
+    break;
+    case 7:
+    conf.count_mode = 3;
+    break;
+    default:
+    conf.count_mode = ((st & (BIT(1)|BIT(2)|BIT(3)))>>1);
+    break;
+  }
+  case tsf_base:
+  conf.bcd = (st & BIT(0));
+  break;
+}
+timer_print_config(timer,field,conf);
+   return 0;
+ }
+ 
--- lab2/utils.c
+++ lab2/utils.c
@@ -3,22 +3,18 @@
 #include <stdint.h>
 
 int(util_get_LSB)(uint16_t val, uint8_t *lsb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  *lsb = val;
+  return 0;
 }
 
 int(util_get_MSB)(uint16_t val, uint8_t *msb) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+ *msb = val >> 8;
+  return 0;
 }
 
 int (util_sys_inb)(int port, uint8_t *value) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
-
-  return 1;
+  uint32_t temp;
+  sys_inb(port, &temp);
+  *value = ( uint8_t ) temp;
+  return 0;
 }
