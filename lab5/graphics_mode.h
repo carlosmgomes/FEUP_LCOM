2029339fc987580b7ab93d45a26a0d4ce1c28c87
LuAs Paulo da Rocha Miranda <up201306340@fe.up.pt>
2020-12-08 16:23:26 +0000

    Update graphics_mode.h

--- lab5/graphics_mode.h
+++ lab5/graphics_mode.h
@@ -15,6 +15,10 @@ vbe_mode_info_t vmi_p;
 
 int vbe_int_10(uint16_t mode);
 void*(vg_init)(uint16_t	mode);
+int draw_pixel(uint16_t x, uint16_t y, uint32_t color);
+int(vg_draw_hline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+int(vg_draw_vline)(uint16_t x, uint16_t y, uint16_t len, uint32_t color);
+int(vg_draw_rectangle)(uint16_t x, uint16_t y, uint16_t width, uint16_t height, uint32_t color);
 
  	
 #endif /* __GRAPHICSMODE_H */
