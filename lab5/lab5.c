962ccaf7b1dbeffc0f7c584a3b3032821d594c12
LuAs Paulo da Rocha Miranda <up201306340@fe.up.pt>
2020-12-08 16:13:14 +0000

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -45,11 +45,46 @@ int(video_test_init)(uint16_t mode, uint8_t delay) {
 
 int(video_test_rectangle)(uint16_t mode, uint16_t x, uint16_t y,
                           uint16_t width, uint16_t height, uint32_t color) {
-  /* To be completed */
-  printf("%s(0x%03X, %u, %u, %u, %u, 0x%08x): under construction\n",
-         __func__, mode, x, y, width, height, color);
 
-  return 1;
+  vg_init(mode);
+
+  int r, ipc_status;
+    uint8_t irq_set;
+    message msg;
+
+    vg_draw_rectangle(x, y, width, height, color);
+  
+    keyboard_subscribe_int(&irq_set);
+
+    while(scancode != 0x81)
+    {
+        if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+          printf("driver_receive failed with: %d", r);
+          continue;
+        }
+        // received notification
+        if (is_ipc_notify(ipc_status))
+        {
+          switch (_ENDPOINT_P(msg.m_source))
+          {
+            case HARDWARE: // hardware interrupt notification
+              if (msg.m_notify.interrupts & irq_set) // subscribed interrupt
+              {
+                  kbc_ih(); // calls the interrupt handler
+                  if(status_error) return 1;
+              }
+             break;
+
+            default:
+             break;
+          }
+        }
+    }
+    keyboard_unsubscribe_int();
+    if(vg_exit()){return 1;}
+
+    return 0;
+
 }
 
 int(video_test_pattern)(uint16_t mode, uint8_t no_rectangles, uint32_t first, uint8_t step) {
