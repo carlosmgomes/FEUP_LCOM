c85cde66b3f4ef91c30512cbb85a9e4d149d5e63
LuAs Paulo da Rocha Miranda <up201306340@fe.up.pt>
2020-12-27 19:11:19 +0000

    added game logic functions

--- proj/game.h
+++ proj/game.h
@@ -26,3 +26,15 @@ void change_turn(Game* game);
 void check_turn_draw(Game* game);
 void check_turn_right(Game *game);
 void check_turn_left(Game *game);
+void init_board(Board *board);
+void fill_board(Game *game);
+int draw_disc_on_coords(Disc *d, int x1, int y1);
+void place_disc_array(Game* game);
+bool check_turn(Game* game);
+bool check_win_vertical(Game* game, int row);
+bool check_win_horizontal(Game* game, int row);
+bool check_win(Game* game, int row);
+bool check_win_diagonal(Game* game);
+
+
+
