78b0075a7a4f65ffe6cee6b48690afb201ca1d98
LuAs Paulo da Rocha Miranda <up201306340@fe.up.pt>
2021-01-04 17:31:50 +0000

    documentation

--- proj/keyboard.h
+++ proj/keyboard.h
@@ -3,24 +3,42 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/** @defgroup keyboard keyboard
+ * @{
+ *
+ * Functions for using the keyboard
+ */
 
 uint32_t siCounter;
 uint8_t scancode_byte[2];
 uint8_t scancode;
 
-
+/**
+ * @brief Subscribes keyboard interrupts
+ * @return Returns bit order in interrupt mask; negative value on failure
+ */
 int (kbc_subscribe_int) (uint8_t *bit_no);
 
+/**
+ * @brief Unsubscribes keyboard interrupts
+ * @return Return 0 upon success and non-zero otherwise
+ */
 int (kbc_unsubscribe_int)();
 
-
+/**
+ * @brief Gets the scan code input from the keyboard.
+ * @return Returns the scan code input from the keyboard
+ */
 int (get_kdb_scancode)(uint8_t *code);
 
 uint8_t size;
 bool kbd_done;
 bool make;
 
-
+/**
+ * @brief handles the interruptions
+ * 
+ */
 void (kbc_ih)();
 
 
